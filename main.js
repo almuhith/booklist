bac1b671559b8821b4df00e8d4a60afbad3c3c63
almuhith <abdullahalmuhith.info@gmail.com>
2022-05-12 16:01:49 +0600

    First push

---
+++ main.js
@@ -0,0 +1,45 @@
+
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const year = document.querySelector('#year');
+const button = document.querySelector('#button');
+const addlist = document.querySelector('#table-list')
+const action = document.querySelector('#remove')
+
+button.addEventListener('click', (e)=> {
+    e.preventDefault()
+    if(title.value == '' || author.value == '' || year.value == '') {
+        alert('Please Enter the value!')
+    }else{
+        const newRow = document.createElement('tr');
+
+        const newTitle = document.createElement('td');
+        newTitle.innerHTML = title.value;
+        newRow.appendChild(newTitle);
+
+        const newAuthor = document.createElement('td');
+        newAuthor.innerHTML = author.value;
+        newRow.appendChild(newAuthor);
+
+        const newYear = document.createElement('td');
+        newYear.innerHTML = year.value;
+        newRow.appendChild(newYear);
+
+        const closeX = document.createElement('td');
+        const button = document.createElement('button');
+        button.setAttribute('id', 'btn');
+        button.innerHTML = 'X';
+        closeX.appendChild(button);
+        newRow.appendChild(closeX);
+        
+
+        addlist.appendChild(newRow);
+
+
+        closeX.addEventListener('click', () => {
+            closeX.parentElement.style.display = 'none';
+        })
+    }
+})
+
+
